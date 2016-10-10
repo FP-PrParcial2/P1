61fa1ecc573118c84335e8c144d268853c74c2b5
alumno <alumno@328-06>
2016-10-10 12:19:27 -0500

    primer problema de proyecto

--- src/pp/problema1/PPProblema1.java
+++ src/pp/problema1/PPProblema1.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package pp.problema1;
-
+import java.util.*;
 /**
  *
  * @author alumno
@@ -15,7 +15,22 @@ public class PPProblema1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code applicatijuygkukon logic here
+     int año, calcular;
     }
-    
+    public static int entradaDato(){
+    int año;
+    Scanner entrada= new Scanner(System.in);
+        System.out.println("Introduce el año");
+       año=entrada.nextInt();
+          
+    return año;   
+   }
+    public static int calcular(int año){  
+        if(año % 4 == 0 && año % 100 != 0 || año % 400 == 0){
+            System.out.println("El año:"+ año +"es bisiesto");  
+       }else{
+            System.out.println("El año:"+ año +"no es bisiesto");
+           }
+    return año;
+    }
 }
