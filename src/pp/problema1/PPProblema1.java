959ab0cb2283c02d67320cae74b4cf84672a1e1a
alumno <alumno@328-06>
2016-10-10 11:45:02 -0500

    primer problema de proyecto

--- src/pp/problema1/PPProblema1.java
+++ src/pp/problema1/PPProblema1.java
@@ -15,7 +15,7 @@ public class PPProblema1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // TODO code applicatijuygkukon logic here
     }
     
 }
